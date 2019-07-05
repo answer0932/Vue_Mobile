f65b28d30241344fd191317b64f4e5113abfffd5
97527027@qq.com <97527027@qq.com>
2019-07-05 12:10:17 +0800

    友情链接 设计稿rem尺寸

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>ACE 的模板</h1>
         <p>
-            <a href="https://www.swiper.com.cn/">Swiper中文网</a>
+            <a href="https://didi.github.io/cube-ui/#/zh-CN">Cube UI</a>
         </p>
         <p>
             <a href="https://daneden.github.io/animate.css/">Animate.css</a>
@@ -12,6 +12,9 @@
         </p>
         <p>
             <a href="http://momentjs.cn/">Moment.js中文网</a>
+        </p>
+        <p>
+            <a href="http://stylus-lang.com/">Stylus</a>
         </p>
     </div>
 </template>
